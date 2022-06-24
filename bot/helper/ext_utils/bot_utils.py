b91172ffee39b593c29fc6d0e7499c22ee6611ff
Jesse Pinkman <95560554+yoitsmejesse@users.noreply.github.com>
2022-06-24 15:53:14 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -71,46 +71,7 @@ class setInterval:
 
     def cancel(self):
         self.stopEvent.set()
-        
-def sendMessage(text: str, bot, update: Update):
-    try:
-        return bot.send_message(update.message.chat_id,
-                            reply_to_message_id=update.message.message_id,
-                            text=text, allow_sending_without_reply=True, parse_mode='HTMl', disable_web_page_preview=True)
-    except RetryAfter as r:
-        LOGGER.warning(str(r))
-        time.sleep(r.retry_after * 1.5)
-        return sendMessage(text, bot, update)
-    except Exception as e:
-        LOGGER.error(str(e))
-        return
-
-def sendMarkup(text: str, bot, update: Update, reply_markup: InlineKeyboardMarkup):
-    try:
-        return bot.send_message(update.message.chat_id,
-                            reply_to_message_id=update.message.message_id,
-                            text=text, reply_markup=reply_markup, allow_sending_without_reply=True,
-                            parse_mode='HTMl', disable_web_page_preview=True)
-    except RetryAfter as r:
-        LOGGER.error(str(r))
-        time.sleep(r.retry_after)
-        return sendMarkup(text, bot, update, reply_markup)
-    except Exception as e:
-        LOGGER.error(str(e))
-
-def editMessage(text: str, message: Message, reply_markup=None):
-    try:
-        bot.edit_message_text(text=text, message_id=message.message_id,
-                              chat_id=message.chat.id,reply_markup=reply_markup,
-                              parse_mode='HTMl', disable_web_page_preview=True)
-    except RetryAfter as r:
-        LOGGER.warning(str(r))
-        time.sleep(r.retry_after * 1.5)
-        return editMessage(text, message, reply_markup)
-    except Exception as e:
-        LOGGER.error(str(e))
-        return
-
+       
 def deleteMessage(bot, message: Message):
     try:
         bot.delete_message(chat_id=message.chat.id,
