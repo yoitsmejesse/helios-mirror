acbefea5d07c18c57dabc6a6de6bc84ff15bf91a
Jesse Pinkman <95560554+yoitsmejesse@users.noreply.github.com>
2022-06-26 18:24:10 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -74,7 +74,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('You're not authorized to use me', context.bot, update.message, reply_markup)
+        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
