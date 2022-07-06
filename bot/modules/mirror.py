13ed809ab04b25b0364224517e04d83d31b0f766
Jesse Pinkman <95560554+yoitsmejesse@users.noreply.github.com>
2022-07-06 09:12:41 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -257,7 +257,7 @@ class MirrorListener:
             msg += f'\n<b>Total Files: </b>{folders}'
             if typ != 0:
                 msg += f'\n<b>Corrupted Files: </b>{typ}'
-            msg += f'\n<b>User: </b>{self.uname}<b> | Elapsed:</b> {get_readable_time(time() - self.message.date.timestamp())}\n\n'
+            msg += f'\n\n<b>User: </b>{self.uname}<b> | Elapsed:</b> {get_readable_time(time() - self.message.date.timestamp())}\n\n'
             if not files:
                 uploadmsg = sendMessage(msg, self.bot, self.message)
             else:
@@ -276,7 +276,7 @@ class MirrorListener:
             if ospath.isdir(f'{DOWNLOAD_DIR}{self.uid}/{name}'):
                 msg += f'\n<b>SubFolders: </b>{folders}'
                 msg += f'\n<b>Files: </b>{files}'
-            msg += f'\n<b>User: </b>{self.uname}<b> | Elapsed:</b> {get_readable_time(time() - self.message.date.timestamp())}'
+            msg += f'\n\n<b>User: </b>{self.uname}<b> | Elapsed:</b> {get_readable_time(time() - self.message.date.timestamp())}'
             buttons = ButtonMaker()
             link = short_url(link)
             buttons.buildbutton("☁️ Drive Link", link)
